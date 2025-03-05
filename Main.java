3b4b39ca6c43b1a16279b23b58539ac39c00eb73
Tamefire <150843062+Tamefire@users.noreply.github.com>
2025-03-05 01:17:18 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
-package localFibonacciSequence;
+package notYourAverageProgram;
+import java.util.Scanner;
 
 public class Main {
-	int[] value = new int{1,0,0};
-	int n = 3;
 	public static void main(String[] args) {
-		for(int i = 0; i < n; i++) {
-			value[2] = value[1]+value[0];
-			System.out.println(value[2]);
+		Scanner scan = new Scanner(System.in);
+		calculations(scan);
+	}
+	public static void calculations(Scanner scan) {
+		int numTests = (askInt("If known, enter the amount of tests completed this semester, if not, enter 0", scan));
+		if(numTests == 0) {
+			//ask for all values, while loop with break for special case. error message if weird percent
+		} else if(numTests>0) {
+			//divide numTest many values by numTest
+		} else {
+			System.out.println("Invalid number, restart program.");
 		}
+	}
+	public static int askInt(String question, Scanner scan){
+		say(question, scan);
+		return scan.nextInt();
+	}
+	public static void say(String statement, Scanner scan) {
+		System.out.println(statement);
 	}
 }
